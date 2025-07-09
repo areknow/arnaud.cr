ecae78f79f729bb7801662e2b3df3f06a2f6d1f7
Arnaud Crowther <arnaudcrowther@gmail.com>
2025-07-08 20:53:36 -0400

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useMouseFollower } from "./use-mouse-follower";
+import "./app.scss";
 
 export const App = () => {
-  useMouseFollower();
-
   return (
-    <div>app</div>
-    // <div style={{ height: "100vh", padding: "20px" }}>
-    //   <h1>Simple Mouse Follower</h1>
-    //   <p>Just import the hook and call it - that's it!</p>
-    //   <p>The blue cube will follow your mouse automatically.</p>
-    // </div>
+    <div className="app">
+      <div className="sidebar">sidebar</div>
+      <div className="main-content">main content</div>
+    </div>
   );
 };
