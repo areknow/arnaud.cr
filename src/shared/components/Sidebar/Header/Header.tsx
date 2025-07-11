e7a64d4bdfc9a143e11c17ea03d44d28b805958e
Arnaud Crowther <arnaudcrowther@gmail.com>
2025-07-10 21:46:31 -0400

    add header

---
+++ src/shared/components/Sidebar/Header/Header.tsx
@@ -0,0 +1,12 @@
+import styles from './header.module.scss';
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.action}></div>
+      <div className={styles.action}></div>
+      <div className={styles.action}></div>
+      <div className={styles.action}></div>
+    </div>
+  );
+};
--- src/shared/components/Sidebar/Sidebar.tsx
+++ src/shared/components/Sidebar/Sidebar.tsx
@@ -1,3 +1,11 @@
+import { Header } from './Header';
+
+import styles from './sidebar.module.scss';
+
 export const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <div className={styles.sidebar}>
+      <Header />
+    </div>
+  );
 };
