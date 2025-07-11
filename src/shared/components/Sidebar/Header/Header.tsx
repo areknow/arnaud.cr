cf152a58e4e5eb3b8ab75cc8b28323d11b1aa5cd
Arnaud Crowther <arnaudcrowther@gmail.com>
2025-07-10 21:55:13 -0400

    sidebar header icons

--- src/shared/components/Sidebar/Header/Header.tsx
+++ src/shared/components/Sidebar/Header/Header.tsx
@@ -3,10 +3,18 @@ import styles from './header.module.scss';
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.action}></div>
-      <div className={styles.action}></div>
-      <div className={styles.action}></div>
-      <div className={styles.action}></div>
+      <div className={styles.action}>
+        <div />
+      </div>
+      <div className={styles.action}>
+        <div />
+      </div>
+      <div className={styles.action}>
+        <div />
+      </div>
+      <div className={styles.action}>
+        <div />
+      </div>
     </div>
   );
 };
