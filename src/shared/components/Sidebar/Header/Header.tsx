7135c10bddc1b675aefe47663785e9c16ac1563a
Arnaud Crowther <arnaudcrowther@gmail.com>
2025-07-12 14:24:49 -0400

    sidebar setup

--- src/shared/components/Sidebar/Header/Header.tsx
+++ src/shared/components/Sidebar/Header/Header.tsx
@@ -1,20 +1,35 @@
+import classNames from 'classnames';
+
+import type { Section } from '../Sidebar';
+
 import styles from './header.module.scss';
 
-export const Header = () => {
+interface HeaderProps {
+  sections: Section[];
+  activeSection: string;
+  onActionClick: (section: string) => void;
+}
+
+export const Header = ({
+  sections,
+  activeSection,
+  onActionClick,
+}: HeaderProps) => {
   return (
     <div className={styles.header}>
-      <div className={styles.action}>
-        <div />
-      </div>
-      <div className={styles.action}>
-        <div />
-      </div>
-      <div className={styles.action}>
-        <div />
-      </div>
-      <div className={styles.action}>
-        <div />
-      </div>
+      {sections.map(section => (
+        <div
+          className={classNames(styles.action, {
+            [styles.active]: activeSection === section.id,
+          })}
+          key={section.id}
+          onClick={() => onActionClick(section.id)}
+        >
+          <div className={styles.icon}>
+            <section.icon size={14} />
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/shared/components/Sidebar/Sidebar.tsx
+++ src/shared/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,57 @@
+import { useState } from 'react';
+
+import { Files, GitPullRequest, type LucideIcon, Search } from 'lucide-react';
+
 import { Header } from './Header';
 
 import styles from './sidebar.module.scss';
 
+export type Section = {
+  icon: LucideIcon;
+  id: string;
+  label: string;
+  content: React.ReactNode;
+};
+
+const SECTIONS: Section[] = [
+  {
+    icon: Files,
+    id: 'explorer',
+    label: 'Explorer',
+    content: <div>file tree</div>,
+  },
+  {
+    icon: Search,
+    id: 'search',
+    label: 'Search',
+    content: <div>assistant?</div>,
+  },
+  {
+    icon: GitPullRequest,
+    id: 'source-control',
+    label: 'Source Control',
+    content: <div>fake git</div>,
+  },
+];
+
 export const Sidebar = () => {
+  const [activeSectionId, setActiveSectionId] = useState<string>('explorer');
+
+  const activeSection = SECTIONS.find(
+    section => section.id === activeSectionId
+  );
+
   return (
     <div className={styles.sidebar}>
-      <Header />
+      <Header
+        sections={SECTIONS}
+        activeSection={activeSectionId}
+        onActionClick={setActiveSectionId}
+      />
+      <div className={styles.content}>
+        <div className={styles.title}>{activeSection?.label}</div>
+        {activeSection?.content}
+      </div>
     </div>
   );
 };
